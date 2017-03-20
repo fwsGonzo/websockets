340855ce88a5c99d3257d9157d6984a2442c9d9d
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2017-03-20 16:08:37 +0100

    Updated with new websocket design

--- service.cpp
+++ service.cpp
@@ -2,19 +2,18 @@
 #include <net/inet4>
 #include <deque>
 #include <net/http/websocket.hpp>
+#include <net/http/ws_connector.hpp>
 
 static std::deque<http::WebSocket_ptr> websockets;
 
-template <typename... Args>
-static http::WebSocket_ptr& new_client(Args&&... args)
+static http::WebSocket_ptr& new_client(http::WebSocket_ptr socket)
 {
   for (auto& client : websockets)
   if (client->is_alive() == false) {
-    return client = 
-        http::WebSocket_ptr(new http::WebSocket(std::forward<Args> (args)...));
+    return client = std::move(socket);
   }
   
-  websockets.emplace_back(new http::WebSocket(std::forward<Args> (args)...));
+  websockets.push_back(std::move(socket));
   return websockets.back();
 }
 
@@ -34,14 +33,14 @@ bool accept_client(net::tcp::Socket remote, std::string origin)
 
 void websocket_service(net::Inet<net::IP4>& inet, uint16_t port)
 {
-  // buffer used for testing
-  static net::tcp::buffer_t BUFFER;
-  static const int          BUFLEN = 1000;
-  BUFFER = decltype(BUFFER)(new uint8_t[BUFLEN]);
-
   fs::memdisk().init_fs(
   [&inet, port] (auto err, auto& filesys) {
     assert(!err);
+
+    // buffer used for testing
+    static net::tcp::buffer_t BUFFER;
+    static const int          BUFLEN = 1000;
+    BUFFER = decltype(BUFFER)(new uint8_t[BUFLEN]);
 
     // load CA certificate
     auto ca_cert = filesys.stat("/test.der");
@@ -50,36 +49,36 @@ void websocket_service(net::Inet<net::IP4>& inet, uint16_t port)
     // load server private key
     auto srv_key = filesys.stat("/server.key");
 
+    using namespace http;
     // Set up a TCP server on port 443
     //static http::Secure_server httpd(
-    //    ca_key, ca_cert, srv_key, inet.tcp());
-    static http::Server httpd(inet.tcp());
+    //    "blabla", ca_key, ca_cert, srv_key, inet.tcp());
+    static Server httpd(inet.tcp());
 
-    /// server ///
-    using namespace http;
-    httpd.on_request(
-    [] (Request_ptr req, Response_writer_ptr writer)
-    {
-      // create client websocket with accept function
-      auto& socket = new_client(std::move(req), std::move(writer), accept_client);
-
-      // if we are still connected, attempt was verified and the handshake was accepted
-      if (socket->is_alive())
+    // Set up server connector
+    static WS_server_connector ws_serve(
+      [] (WebSocket_ptr ws)
       {
-        socket->on_read =
-        [] (const char* data, size_t len) {
-          (void) data;
-          (void) len;
-          printf("WebSocket on_read: %.*s\n", len, data);
-        };
-        
-        //socket->write("THIS IS A TEST CAN YOU HEAR THIS?");
-        for (int i = 0; i < 1000; i++)
-            socket->write(BUFFER, BUFLEN, http::WebSocket::BINARY);
-        
-        //socket->close();
-      }
-    });
+        auto& socket = new_client(std::move(ws));
+        // if we are still connected, attempt was verified and the handshake was accepted
+        if (socket->is_alive())
+        {
+          socket->on_read =
+          [] (const char* data, size_t len) {
+            (void) data;
+            (void) len;
+            printf("WebSocket on_read: %.*s\n", len, data);
+          };
+          
+          //socket->write("THIS IS A TEST CAN YOU HEAR THIS?");
+          for (int i = 0; i < 1000; i++)
+              socket->write(BUFFER, BUFLEN, http::WebSocket::BINARY);
+          
+          //socket->close();
+        }
+      },
+      accept_client);
+    httpd.on_request(ws_serve);
     httpd.listen(port);
     /// server ///
   });
@@ -87,7 +86,7 @@ void websocket_service(net::Inet<net::IP4>& inet, uint16_t port)
 
   /// client ///
   static http::Client client(inet.tcp());
-  http::WebSocket::connect(client, "ws://10.0.0.42", "ws://10.0.0.1:8001/", 
+  http::WebSocket::connect(client, "ws://10.0.0.1:8001/", 
   [] (http::WebSocket_ptr socket)
   {
     if (!socket) {
@@ -110,12 +109,13 @@ void Service::start()
       {  10, 0,  0,  1 },  // Gateway
       {  10, 0,  0,  1 }); // DNS
 
+  
   websocket_service(inet, 8000);
 }
 #include <profile>
 #include <timers>
 void Service::ready()
 {
-  auto stats = ScopedProfiler::get_statistics();
-  printf("%.*s\n", stats.size(), stats.c_str());
+  //auto stats = ScopedProfiler::get_statistics();
+  //printf("%.*s\n", stats.size(), stats.c_str());
 }
